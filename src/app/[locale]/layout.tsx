023dac870d02b8830e3936260d01058280c6a3b3
Pedrolopespro <contaslopeshpl@gmail.com>
2026-07-10 22:15:51 -0300

    Add Microsoft Clarity analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import { notFound } from "next/navigation";
 import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
@@ -82,6 +83,17 @@ export default async function LocaleLayout({
       <body suppressHydrationWarning className="min-h-screen flex flex-col">
         {/* Global JSON-LD */}
         <JsonLd data={homeSchemas()} />
+
+        {/* Microsoft Clarity */}
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xkjdgicz0s");
+          `}
+        </Script>
 
         <NextIntlClientProvider>
           <QuoteModalProvider>
